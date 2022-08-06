1c8d3cbaaba5006067ed8e12355b20c233122da0
Alex Camacho <camacho.ale94@gmail.com>
2022-08-06 01:01:25 -0500

    Project base

---
+++ who is that pokemon/PokemonViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  who is that pokemon
+//
+//  Created by Alex Camacho on 01/08/22.
+//
+
+import UIKit
+
+class PokemonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
